0042ddb4535296bed5d4cfef6cd02eff01624717
Aubrey Salmins <aubreysalmins@gmail.com>
2023-05-03 10:39:02 +0100

    Project setup

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Application < Sinatra::Base
+  # This allows the app code to refresh
+  # without having to restart the server.
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/names' do
+    return "Julia, Mary, Karim"
+  end
+
+  post '/sort-names' do
+    names = params[:names]
+
+    return names.split(",").sort.join(",")
+  end
+end
---
+++ spec/integration/app_spec.rb
@@ -0,0 +1,31 @@
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+
+describe Application do
+  # This is so we can use rack-test helper methods.
+  include Rack::Test::Methods
+
+  # We need to declare the `app` value by instantiating the Application
+  # class so our tests work.
+  let(:app) { Application.new }
+
+  context "GET /names" do
+    it 'returns 200 OK' do
+      response = get('/names?')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq("Julia, Mary, Karim")
+    end
+  end
+
+  context "POST/sort-names" do
+    it 'returns 200 OK' do
+      # Assuming the post with id 1 exists.
+      response = post('/sort-names?names=Joe,Alice,Zoe,Julia,Kieran')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq("Alice,Joe,Julia,Kieran,Zoe")
+    end
+  end
+end
